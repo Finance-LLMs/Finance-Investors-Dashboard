d470b0fda60919de0000c123e40beb653b836757
akshatparmar2634 <akshat22050@iiitd.ac.in>
2025-05-31 16:37:42 +0530

    misc

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const axios = require('axios');
 const { PythonShell } = require('python-shell');
 const FormData = require('form-data');
 require('dotenv').config();
-
+ 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
